8263758a19933355da14ec7afc16a933a6a7879c
Venera <osmven@gmail.com>
2022-07-09 17:09:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int password;
+        do {
+            System.out.println("Write password");
+            password = read.nextInt();
+        }
+            while (password != 8819) ;
+
+
+
+    }
+
+}
+
+
+
+
+
+
